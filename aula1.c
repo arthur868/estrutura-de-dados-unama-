bb105e613e9c8f0cc228b1c5df60e273033a91e2
arthur868 <ar7981534@gmail.com>
2024-08-21 14:41:50 +0000

    add..

--- aula1.c
+++ aula1.c
@@ -1,4 +1,16 @@
 #include <stdio.h>
+typedef struct(
+    int peso;  // peso em quilogramas
+    int altura; // altura em centimetros 
+)PesoAltura;
+
+int main() {
+    pesoaltura pessoa;
+    pessoa peso = 95;
+    altura pessoa = 180;
+    int n = 1 
+    printf('peso: %i, altura %i\in', pessoa.peso, pessoa.altura)
+}return 0;
 
 int main(){
     printf('HelloWorld\in');
